cfd81f2c57b97d78dba7f8f1e85a95118ec554e9
iripoli <iripoli96@gmail.com>
2020-07-17 13:40:19 +0200

    fixed no dispatch in useEffect error

--- src/containers/Catalogue-Page/Catalogue-Page.tsx
+++ src/containers/Catalogue-Page/Catalogue-Page.tsx
@@ -1,14 +1,23 @@
 import React, { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchAllPhones } from "../../redux/actions/fetchPhones.actions";
 
+interface RootState {
+  Phones_Reducer: any;
+}
+
 const CataloguePage = () => {
+  const phones = useSelector(
+    (state: RootState) => state.Phones_Reducer.phoneList
+  );
+  console.log(phones);
+
   const dispatch = useDispatch();
   useEffect(() => {
     (async () => {
       await dispatch(fetchAllPhones());
     })();
-  }, []);
+  }, [dispatch]);
 
   return <div>Catalogue Page</div>;
 };
