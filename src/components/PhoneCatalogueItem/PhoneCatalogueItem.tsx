b876705b7af07eb54e7abb07d4c32e3f2e179a9f
iripoli <iripoli96@gmail.com>
2020-07-19 02:15:44 +0200

    added list view styles to catalogue

--- src/components/PhoneCatalogueItem/PhoneCatalogueItem.tsx
+++ src/components/PhoneCatalogueItem/PhoneCatalogueItem.tsx
@@ -9,12 +9,32 @@ interface Data {
 }
 
 const PhoneCatalogueItem = ({ phoneData, viewList }: Data) => {
-  const { _id, images, name } = phoneData;
+  const {
+    _id,
+    images,
+    name,
+    manufacturer,
+    price,
+    screen,
+    ram,
+    processor,
+  } = phoneData;
   return (
-    <Link to={`/catalogue/${_id}`} className="itemName">
+    <Link
+      to={`/catalogue/${_id}`}
+      className={!viewList ? "" : "container__item"}
+    >
       <div className={viewList ? "itemContainer__list" : "itemContainer__grid"}>
         <img src={images} alt={`Front of ${name}`} className="itemImage" />
         <h1 className="itemName">{name}</h1>
+        {!viewList ? null : (
+          <>
+            <h1 className="itemName">{manufacturer}</h1>
+            <h1 className="itemName">{screen}</h1>
+            <h1 className="itemName">{processor}</h1>
+            <h1 className="itemName">{ram}gb</h1>
+          </>
+        )}
       </div>
     </Link>
   );
